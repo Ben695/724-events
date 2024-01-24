f9dd4c1fe23f3cacb0a5516c59dce56c70659893
Benjamin <benkessas.benjamin@gmail.com>
2024-01-24 17:30:35 +0100

    Test unitaire getMonth

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,18 @@
-/**
- * 
- */
+import { getMonth } from ".";
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+        it("the function returns 'janvier' for the date '2022-01-01'", () => {
+            const date = new Date("2022-01-01");
+            const result = getMonth(date);
+            expect(result).toEqual("janvier");
         });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+
+        it("the function returns 'juillet' for the date '2022-07-08'", () => {
+            const date = new Date("2022-07-08");
+            const result = getMonth(date);
+            expect(result).toEqual("juillet");
         });
     });
-})
+});
 
